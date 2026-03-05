42a5b4c68a2f1c72292e93242c333f6704fafe30
Dhivya Dharshini A <145836214+dhivyacr@users.noreply.github.com>
2026-03-05 09:44:14 +0530

    Refactor EvenDigits to Solution and improve findNumbers
    Refactored the EvenDigits class to a Solution class, updated the findNumbers method to count numbers with even digits, and removed unused methods.

--- Arrays/find-numbers-with-even-number-of-digits.java
+++ Arrays/find-numbers-with-even-number-of-digits.java
@@ -1,60 +1,39 @@
-package com.dd;
-// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
-public class EvenDigits {
-    public static void main(String[] args) {
-        int[] nums = {12,345,2,6,7896};
-//        System.out.println(findNumbers(nums));
-
-        System.out.println(digits2(-345678));
+import java.util.*;
+class Solution {
+    public int findNumbers(int[] list) {
+        int count=0;
+        for(int i=0;i<list.length;i++){
+            int digit = (int)Math.log10(Math.abs(list[i]))+1;
+            if(digit%2==0){
+                count++;
+            }
+        }
+       return count;
     }
-    static int findNumbers(int[] nums) {
-        int count = 0;
-        for(int num : nums) {
-            if (even(num)) {
+}
+        /*
+        public int findNumbers(int[] list){
+        int count=0;
+        for(int i=0;i<list.length;i++){
+            int element=list[i];
+            if(even(element)){
                 count++;
             }
         }
         return count;
     }
-
-    // function to check whether a number contains even digits or not
-    static boolean even(int num) {
-        int numberOfDigits = digits(num);
-        /*
-        if (numberOfDigits % 2 == 0) {
+    boolean even (int element){
+        int count = (int) Math.log10(element)+1;
+        if(count%2==0){
             return true;
         }
         return false;
-         */
-        return numberOfDigits % 2 == 0;
     }
-
-    static int digits2(int num) {
-        if (num < 0) {
-            num = num * -1;
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
+        ArrayList<Integer>list=new ArrayList<>();
+        while(sc.hasNextInt()){
+            list.add(sc.nextInt());
         }
-        return (int)(Math.log10(num)) + 1;
     }
-
-    // count number of digits in a number
-    static int digits(int num) {
-
-        if (num < 0) {
-            num = num * -1;
-        }
-
-        if (num == 0) {
-            return 1;
-        }
-
-        int count = 0;
-        while (num > 0) {
-            count++;
-            num = num / 10; // num /= 10
-        }
-
-        return count;
-    }
-
-
-}
+}*/
